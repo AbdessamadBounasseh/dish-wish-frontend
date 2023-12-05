c01a655456f8e58c19269376e71ac64f2d5d5468
FAYCAL EL OURRAT <faycal.elourrat@uit.ac.ma>
2023-12-05 13:15:30 +0100

    cleaned project

--- app/src/main/java/uit/ensak/dish_wish_frontend/MainActivity.java
+++ app/src/main/java/uit/ensak/dish_wish_frontend/MainActivity.java
@@ -1,34 +1,11 @@
 package uit.ensak.dish_wish_frontend;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.widget.Toast;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.time.Instant;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import uit.ensak.dish_wish_frontend.service.ApiService;
-import uit.ensak.dish_wish_frontend.api.RetrofitClient;
-import uit.ensak.dish_wish_frontend.databinding.ActivityMainBinding;
-import uit.ensak.dish_wish_frontend.model.Client;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
-
-//    @Override
+    //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
 //
@@ -56,76 +33,4 @@ public class MainActivity extends AppCompatActivity {
 //        NavigationUI.setupWithNavController(navigationView, navController);
 //    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        Long userId = 1L;
-
-        ApiService apiService = RetrofitClient.getApiService();
-        Call<Client> call = apiService.getUser(userId);
-
-        Log.d(TAG, call.toString());
-
-        call.enqueue(new Callback<Client>() {
-            @Override
-            public void onResponse(Call<Client> call, Response<Client> response) {
-                if (response.isSuccessful()) {
-                    Client user = response.body();
-                    if (user != null) {
-                        binding.id.setText(String.valueOf(user.getId()));
-                        binding.email.setText(user.getEmail());
-                        binding.password.setText(user.getPassword());
-                    }
-                } else {
-                    // Handle unsuccessful response
-                    Toast.makeText(MainActivity.this, response.errorBody().toString(), Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Client> call, Throwable t) {
-                // Handle network or API call failure
-                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-        Client client = new Client(5L,"test@example.com", "password", "John", "Doe", "123 Street", "1234567890", "image.jpg", null, null);
-
-
-        Call<Void> callapi = apiService.sendData(client);
-        callapi.enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> callapi, Response<Void> response) {
-                if (response.isSuccessful()) {
-
-                } else {
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                // Handle failure
-            }
-        });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
