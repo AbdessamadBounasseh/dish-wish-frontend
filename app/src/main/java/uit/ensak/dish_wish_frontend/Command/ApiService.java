fa8d9fced527404fab061f43e9291669a9532592
amine elkaaba <mohamedamine.elkaaba@uit.ac.ma>
2023-12-13 16:27:14 +0100

    order a dish

--- app/src/main/java/uit/ensak/dish_wish_frontend/Command/ApiService.java
+++ app/src/main/java/uit/ensak/dish_wish_frontend/Command/ApiService.java
@@ -5,9 +5,11 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import uit.ensak.dish_wish_frontend.Models.Client;
+import uit.ensak.dish_wish_frontend.Models.Command;
 
 public interface ApiService {
     @GET("clients/{id}")
@@ -15,5 +17,8 @@ public interface ApiService {
 
     @POST("clients/add")
     Call<Void> sendData(@Body Client client);
+
+    @POST("commands/create")
+    Call<Void> createCommand(@Header("Authorization") String authToken, @Body Command command);
 
 }
