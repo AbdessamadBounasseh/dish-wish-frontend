37fd31f822aa1a96c369d62e378a3429f4fd6254
FAYCAL EL OURRAT <faycal.elourrat@uit.ac.ma>
2023-12-20 23:14:14 +0100

    corrected creatAccount

--- app/src/main/java/uit/ensak/dish_wish_frontend/connect.java
+++ app/src/main/java/uit/ensak/dish_wish_frontend/connect.java
@@ -5,19 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -46,7 +40,7 @@ public class connect extends AppCompatActivity {
         newacc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent1= new Intent(connect.this,createAcciunt.class);
+                Intent intent1= new Intent(connect.this, createAccount.class);
                 startActivity(intent1);
             }
         });
