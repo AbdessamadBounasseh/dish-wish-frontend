23251804fff200229f149963b51538abe3551b8b
FAYCAL EL OURRAT <faycal.elourrat@uit.ac.ma>
2024-01-11 19:04:35 +0100

    sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,13 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "FAYCAL-EL_dish-wish-frontend")
+        property("sonar.organization", "faycal-el")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
