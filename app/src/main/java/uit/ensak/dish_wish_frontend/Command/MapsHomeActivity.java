8759a96ae5be3019599eb7ec43279e595d4f07b5
FAYCAL EL OURRAT <faycal.elourrat@uit.ac.ma>
2024-01-12 18:14:22 +0100

    readded menu

--- app/src/main/java/uit/ensak/dish_wish_frontend/Command/MapsHomeActivity.java
+++ app/src/main/java/uit/ensak/dish_wish_frontend/Command/MapsHomeActivity.java
@@ -63,7 +63,7 @@ import uit.ensak.dish_wish_frontend.Models.Command;
 import uit.ensak.dish_wish_frontend.Models.Proposition;
 import uit.ensak.dish_wish_frontend.R;
 import uit.ensak.dish_wish_frontend.databinding.ActivityMapsHomeBinding;
-import uit.ensak.dish_wish_frontend.search_folder.filter_by_name_or_city;
+import uit.ensak.dish_wish_frontend.filter_by_name_or_city;
 //import uit.ensak.dish_wish_frontend.filter_by_name_or_city;
 
 public class MapsHomeActivity extends FragmentActivity implements OnMapReadyCallback {
