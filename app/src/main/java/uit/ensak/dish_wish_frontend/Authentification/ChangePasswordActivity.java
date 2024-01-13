a60f789582bb7efe18958d192263cee944143937
khaoulazaar <khawlazaar11@gmail.com>
2024-01-13 01:02:57 +0100

    some changes

---
+++ app/src/main/java/uit/ensak/dish_wish_frontend/Authentification/ChangePasswordActivity.java
@@ -0,0 +1,16 @@
+package uit.ensak.dish_wish_frontend.Authentification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import uit.ensak.dish_wish_frontend.R;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
