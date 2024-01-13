bca8bdde2f341abf77c0cc0a061f230046aa09b5
Chaymaa AHCINE <chaymaa@mail.com>
2024-01-13 21:49:17 +0100

    add rating activity

---
+++ app/src/main/java/uit/ensak/dish_wish_frontend/Profil/search_profile.java
@@ -0,0 +1,16 @@
+package uit.ensak.dish_wish_frontend.Profil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import uit.ensak.dish_wish_frontend.R;
+
+public class search_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_profile);
+    }
+}
